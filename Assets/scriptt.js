aaa13373c98f645aee548a81d6fb8997b862fb36
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-03-17 23:18:37 -0500

    started to restructure code restructured html page got api on js to work need to get forecast to display

---
+++ Assets/scriptt.js
@@ -0,0 +1,5 @@
+// Global variables
+var APIKey = "1f424de6f8c55f73372510aac5d23b26";
+// TODO API key not working 400 error not sure why maybe add lon adn lat to link??
+var currentCity = "";
+var lastCity ="";
